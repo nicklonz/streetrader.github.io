d3ee52786746648abf44c123e7b54ce17e9a1022
streetrader <nicklonz@gmail.com>
2016-10-12 09:56:10 -0400

    Added monkey.js

--- jsd920_class_repo/week4/class-07-objects-json/monkey.js
+++ jsd920_class_repo/week4/class-07-objects-json/monkey.js
@@ -13,4 +13,33 @@ Create 3 monkeys total. Make sure all 3 monkeys have all properties set and meth
 Exercise your monkeys by retrieving their properties and using their methods. Practice using both syntaxes
 for retrieving properties (dot notation and brackets).
 
-*/
+ var Monkey = function (name, species,foodsEaten,eatSomething) {
+    this.name = name;
+    this.species = species;
+    this.foodsEaten = foodsEaten;
+    this.eatSomething = extraFood;
+  }
+
+  var magila = new Monkey('Magila', 'Mandrill','banannas','beans')
+  
+  console.log(magila) 
+  */
+  
+  var Monkey = function(name,species,foodsEaten) {
+      this.name = name,
+      this.species = species,
+      this.foodsEaten = foodsEaten
+      this.eatSomething = function(){
+        console.log("Hello my name is " + this.name + " and I've eaten " + foodsEaten)
+      }
+  }
+  
+  var bobo = new Monkey("Bobo", "snake-monkey","bananna")
+  
+  bobo.name
+  bobo.eatSomething()
+  
+  
+
+
+
