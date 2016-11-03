5cdc16dc560c00aa28c84a1cdbe28b2510ae81e5
streetrader <nicklonz@gmail.com>
2016-11-03 18:35:25 -0400

    Added Week 7 Stuff

--- jsd920_class_repo/week7/class-13-advanced-apis/starter-code/app.js
+++ jsd920_class_repo/week7/class-13-advanced-apis/starter-code/app.js
@@ -1,4 +1,61 @@
 $(function() {
   // DOM is now ready
+  _500px.init({
+    sdk_key: '0e2bfe357251c6b2a05e610ca5e66f09fe72fcd5'
+  });
 
+     $('#login').click(function() {
+        console.log("login clicked");
+        _500px.login();
+    });
+
+    _500px.on('authorization_obtained', function() {
+        // Successful OAuth login!
+        $('.sign-in-view').hide();
+        $('.image-results-view').show();
+
+        if (navigator.geolocation) {
+            // if it is use the getCurrentPosition method to retrieve the Window's location
+            navigator.geolocation.getCurrentPosition(function(position) {
+              var lat = position.coords.latitude;
+              var long = position.coords.longitude;
+
+              console.log('lat: ', lat);
+              console.log('long: ', long);
+
+              var radius = '25mi';
+
+              var searchOptions = {
+                  geo: lat + "," + long + "," + radius,
+                  only: 'Landscapes',
+                  image_size: 3,
+                  rpp: 28,
+                  sort: 'highest_rating'
+              };
+
+                _500px.api('/photos/search', searchOptions, function(response) {
+                    console.log(response);
+                    if (response.data.photos.length == 0) {
+                        alert('no photos found');
+                    } else {
+                        response.data.photos.forEach(function(photo) {
+                          var image_url = photo.image_url;
+                          var image_element = $('<img>');
+                          image_element.attr('src', image_url);
+                          $('div.images').append(image_element);
+                        });
+
+                        $("images").each(function() {
+                            $(this).append('.images' + response.data.photos.image_url + '.images').attr('src');
+                        });
+
+                        //custom function
+                    }
+                });
+
+            });
+        } else {
+            $('.images').append('Sorry, the browser does not support geolocation');
+        }
+    });
 });
