0eef370b2281da226ab0637d89c1a663742707de
streetrader <nicklonz@gmail.com>
2016-11-03 18:36:39 -0400

    Added 99_bottles Stuff

--- jsd920_class_repo/week4/homework/99_bottles_DOM/main.js
+++ jsd920_class_repo/week4/homework/99_bottles_DOM/main.js
@@ -1,13 +1,21 @@
-var bottles = 99,bottle="bottles",text="";
+var bottles = 200,bottle="bottles",text="";
 window.onload=function() {
-  var count = document.getElementById('count');  
-  while (bottles > 0) {
-    if (bottles==1) bottle="bottle"; 
-    text = "<p>"+bottles+" "+bottle+" of beer on the wall, "+bottles+" "+bottle+" of beer.<br/>";
-    bottles--;
-    if (bottles==0) bottles="no more"; 
-    text += "Take one down and pass it around, "+bottles+" bottles of beer on the wall.</p>"
-    count.innerHTML += text;
-  }
-  count.innerHTML += "<p>No more bottles of beer on the wall, no more bottles of beer.<br>Better go get some coffee!!!</p>"
-} 
+  $('button#btn').on('click', function(event){
+    event.preventDefault();
+
+    var bottles = $('input#bottles').val(); 
+    var team = $('select#team').val(); 
+
+    console.log('The team choosen is ',team)
+    while (bottles > 0) {
+      if (bottles==1) bottle="bottle"; 
+      text = "<p>"+bottles+" "+bottle+" of beer on the wall, "+bottles+" "+bottle+" of beer.<br/>";
+      bottles--; //this is the same as bottles = bottles - 1;
+      if (bottles==0) bottles="no more"; 
+      text += "Take one down and pass it around, "+bottles+" bottles of beer on the wall.</p>"
+      count.innerHTML += text;
+    }
+    count.innerHTML += "<p>No more bottles of beer on the wall, no more bottles of beer.<br>Better go get some coffee!!!</p>"
+  });
+
+};
