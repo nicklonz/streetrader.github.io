8c604d9d229dd51d18954cdded7639bd6a928b72
streetrader <nicklonz@gmail.com>
2016-11-19 12:24:58 -0500

    MLB Final Project w Bugs

--- jsd920_class_repo/projects/mlb_angular_project/script.js
+++ jsd920_class_repo/projects/mlb_angular_project/script.js
@@ -1,3 +1,10 @@
+	function TodoCtrl($scope) {
+ 
+    $scope.total = function(){
+        return $scope.one * $scope.two;
+    };
+}
+
 	// create the module and name it scotchApp
 	var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
@@ -17,6 +24,12 @@
 				controller  : 'aboutController'
 			})
 
+			// route for the data page
+			.when('/data', {
+				templateUrl : 'pages/data.html',
+				controller  : 'dataController'
+			})
+
 			// route for the contact page
 			.when('/contact', {
 				templateUrl : 'pages/contact.html',
@@ -27,11 +40,16 @@
 	// create the controller and inject Angular's $scope
 	scotchApp.controller('mainController', function($scope) {
 		// create a message to display in our view
-		$scope.message = 'Everyone come and see how good I look!';
+		$scope.message = 'Thank You for visiting!';
+		$scope.games_played = '162';
 	});
 
 	scotchApp.controller('aboutController', function($scope) {
 		$scope.message = 'Look! I am an About page.';
+	});
+
+	scotchApp.controller('dataController', function($scope) {
+		$scope.message = 'Welcome to the Data page.';
 	});
 
 	scotchApp.controller('contactController', function($scope) {
