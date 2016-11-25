466dc6262125c91d8639a6df32f3c0dafbfa4ba3
streetrader <nicklonz@gmail.com>
2016-11-25 12:31:07 -0500

    Added Fixes

--- jsd920_class_repo/projects/mlb_angular_project/script.js
+++ jsd920_class_repo/projects/mlb_angular_project/script.js
@@ -1,9 +1,10 @@
 
-	// create the module and name it scotchApp
-	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+	// create the module and name it mlbApp
+	var mlbApp = angular.module('mlbApp', ['ngRoute']);
+	var movieApp = angular.module('movieApp', []);
 
 	// configure our routes
-	scotchApp.config(function($routeProvider) {
+	mlbApp.config(function($routeProvider) {
 		$routeProvider
 
 			// route for the home page
@@ -24,10 +25,10 @@
 				controller  : 'dataController'
 			})
 
-			// route for the weather page
-			.when('/weather', {
-				templateUrl : 'pages/weather.html',
-				controller  : 'weatherController'
+			// route for the movie page
+			.when('/movies', {
+				templateUrl : 'pages/movies.html',
+				controller  : 'moviesController'
 			})
 
 			// route for the contact page
@@ -38,7 +39,7 @@
 	});
 
 	// create the controller and inject Angular's $scope
-	scotchApp.controller('mainController', function($scope) {
+	mlbApp.controller('mainController', function($scope) {
 		// create a message to display in our view
 		$scope.message = 'Thank You for visiting!';
 		/*
@@ -82,87 +83,19 @@
 		};
 	});
 
-	scotchApp.controller('aboutController', function($scope) {
+	mlbAppApp.controller('aboutController', function($scope) {
 		$scope.message = 'Look! I am an About page.';
 	});
 
-	scotchApp.controller('dataController', function($scope) {
+	mlbApp.controller('dataController', function($scope) {
 		$scope.message = 'Welcome to the Data page.';
 	});
 
-	scotchApp.controller('weatherController', function($scope) {
-		$scope.message = 'Welcome to the Weather page.';
-		  // var apiKey = 'd5f81af7e565653bdaca9a5f863d08a0'
-
-		  // // var apiKey = 
-		  // var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?appid=' + apiKey + '&units=imperial&q=';
-
+	mlbApp.controller('moviesController', function($scope) {
+		$scope.message = 'Welcome to the Movies API page.';
 		 
-		  // $.get(weatherUrl + 'New York City')
-		  //   .fail(function (xhr) {
-		  //     console.log(xhr);
-		  //   })
-		  //   .done(function (response) {
-		  //     console.log(response);
-
-		  //     var temp = response.main.temp;
-		  //     var humidity = response.main.humidity;
-		  //     var windSpeed = response.wind.speed;
-
-		  //   $('#nyc-weather')
-		  //     .append('<p>Temperature: ' + temp + '</p>')
-		  //     .append('<p>Humidity: ' + humidity + '</p>')
-		  //     .append('<p>Wind Speed: ' + windSpeed + '</p>')
-
-		  //   });
-		    
-		  //   $('#weather-form').submit(function (event) {
-		  //     event.preventDefault();
-
-		  //     // get user input
-		  //     var city = $('#city').val();
-		  //     var state = $('#state').val();
-
-		  //     $.ajax({
-		  //       url: weatherUrl + city + ',' + state,
-		  //       type: 'GET',
-		  //       success: function (response) {
-		  //         // pipe AJAX reponse to outside function for cleaner code
-		  //         outputWeather(response);
-		  //       },
-		  //       error: function (xhr) {
-		  //         console.log(xhr);
-		  //       }
-		  //     });
-		  //   })
-
-		  //   function outputWeather (response) {
-		  //     console.log(response);
-
-		  //     var city = response.name;
-		  //     var temp = response.main.temp;
-		  //     var humidity = response.main.humidity;
-		  //     var windSpeed = response.wind.speed;
-
-		  //     // change background color
-		  //     colorBackground(temp);
-
-		  //     $('#weather-output')
-		  //       .empty() // be sure to clear out any data from previous searches!
-		  //       .append('<p>City: ' + city + '</p>')
-		  //       .append('<p>Temperature: ' + temp + '</p>')
-		  //       .append('<p>Humidity: ' + humidity + '</p>')
-		  //       .append('<p>Wind Speed: ' + windSpeed + '</p>')
-
-		  //   function colorBackground (temp) {
-		  //     if (temp > 75) {
-		  //       $('body').css('background', 'red');
-		  //     } else {
-		  //       $('body').css('background', 'blue');
-		  //     }
-		  //   }
 	});
 
-	scotchApp.controller('contactController', function($scope) {
+	mlbApp.controller('contactController', function($scope) {
 		$scope.message = 'Visit nicklonz.com to see more development work.';
 	});
